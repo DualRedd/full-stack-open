e6da593a48fa68191123aefbc76ef5f5f814382b
Jonni Haapiainen <jonni.haapiainen@gmail.com>
2026-02-01 17:22:11 +0200

    Remove unused variable

--- part3/phonebook_backend/index.js
+++ part3/phonebook_backend/index.js
@@ -3,30 +3,6 @@ import morgan from 'morgan'
 import './db.js'
 import Person from './models/person.js'
 
-let data = [
-    { 
-      "id": "1",
-      "name": "Arto Hellas", 
-      "number": "040-123456"
-    },
-    { 
-      "id": "2",
-      "name": "Ada Lovelace", 
-      "number": "39-44-5323523"
-    },
-    { 
-      "id": "3",
-      "name": "Dan Abramov", 
-      "number": "12-43-234345"
-    },
-    { 
-      "id": "4",
-      "name": "Mary Poppendieck", 
-      "number": "39-23-6423122"
-    }
-]
-
-
 const app = express()
 app.use(express.static('dist'))
 app.use(express.json())
